3d5cacdb0b41ab1ce597d2b1619376d281cddd89
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-25 17:55:24 -0300

    test: test parse function for TOML format

--- tests/test_toml.py
+++ tests/test_toml.py
@@ -32,3 +32,30 @@ drink = "vanilla McShake"
 dessert = "apple pie"'''
 
         assert output_data == expected_output
+
+    def test_parse(self):
+        "return Python object from TOML string"
+
+        input_data = '''[[pedido]]
+sandwich = "Big Mac"
+drink = "Coca Cola"
+dessert = "vanilla ice cream"
+
+[[pedido]]
+sandwich = "Mc Chicken"
+drink = "vanilla McShake"
+dessert = "apple pie"'''
+        output_data = parse(input_data, "toml")
+
+        expected_output = {"pedido": [
+            {
+                "sandwich": "Big Mac",
+                "drink": "Coca Cola",
+                "dessert": "vanilla ice cream"
+            },
+            {
+                "sandwich": "Mc Chicken",
+                "drink": "vanilla McShake",
+                "dessert": "apple pie"
+            }]
+        }
