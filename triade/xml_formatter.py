d795b37a954e884c99d37af7e7b9350f71231e85
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-30 21:57:52 -0300

    chore: create class boilerplate

---
+++ triade/xml_formatter.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+
+from element import Element
+
+
+class XML:
+    @staticmethod
+    def loads(input_data: str) -> object:
+        "Parse XML string into Python object"
+        pass
+
+    @staticmethod
+    def dumps(input_data: object) -> str:
+        "Converts Python object into XML string"
+        pass
