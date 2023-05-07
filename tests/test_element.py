c47cbd42cb5e10f37798452e1fec0cf0982c594c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-05-07 19:14:22 -0300

    test: add test class and docstring

--- tests/test_element.py
+++ tests/test_element.py
@@ -1,31 +1,34 @@
 from triade.element import Element
 
 
-def test_create_with_dictionary():
-    Element({"tag": "span", "text": "some text"})
+class TestElement:
+    "Test Element class"
 
-    Element({"tag": "div",
-             "children": [
-                 {"tag": "span", "text": "first span"},
-                 {"tag": "span", "text": "second span"}]})
+    def test_create_with_dictionary(self):
+        Element({"tag": "span", "text": "some text"})
 
-def test_create_with_positional_args():
-    Element("span", None, None, "some text")
+        Element({"tag": "div",
+                 "children": [
+                     {"tag": "span", "text": "first span"},
+                     {"tag": "span", "text": "second span"}]})
 
-    Element("main", {"class": "content"},
-            [Element("span", None, None, "first span"),
-             Element("span", None, None, "second span")])
+    def test_create_with_positional_args(self):
+        Element("span", None, None, "some text")
 
-def test_create_with_named_args():
-    Element(tag="span", text="some text")
+        Element("main", {"class": "content"},
+                [Element("span", None, None, "first span"),
+                 Element("span", None, None, "second span")])
 
-    Element(tag="main", attributes={"class": "content"},
-            children=[Element(tag="span", text="first span"),
-                      Element(tag="span", text="second span")])
+    def test_create_with_named_args(self):
+        Element(tag="span", text="some text")
 
-def test_create_with_positional_and_named_args():
-    Element("div", {"class": "container"},
-            children=[Element("p", text="This is the first paragraph"),
-                      Element("p", text="This is the second one")])
+        Element(tag="main", attributes={"class": "content"},
+                children=[Element(tag="span", text="first span"),
+                          Element(tag="span", text="second span")])
+
+    def test_create_with_positional_and_named_args(self):
+        Element("div", {"class": "container"},
+                children=[Element("p", text="This is the first paragraph"),
+                          Element("p", text="This is the second one")])
 
     Element("span", text="some text")
